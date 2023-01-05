0439143a116b857b9f0877501e635e7a806ab098
Lukas Hahmann <tfr.didi@gmail.com>
2023-01-05 23:11:07 +0100

    test-slider hinzugefügt

--- src/index.js
+++ src/index.js
@@ -4,3 +4,30 @@ import "./css/main.css";
 
 // Say hello
 console.log("🦊 Hello! Edit me in src/index.js");
+
+
+// timeline
+
+// import Swiper JS
+import Swiper from './js/swiper';
+
+var swiper = new Swiper(".swiper-container", {
+    direction: 'vertical',
+  loop: false,
+  speed: 1600,
+  pagination: '.swiper-pagination',
+  paginationBulletRender: function (swiper, index, className) {
+    var year = document.querySelectorAll('.swiper-slide')[index].getAttribute('data-year');
+    return '<span class="' + className + '">' + year + '</span>';
+  },
+  paginationClickable: true,
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
+  breakpoints: {
+    768: {
+      direction: 'horizontal',
+    }
+  }
+});
+  
+  console.log("🦊 Hello from didi");
