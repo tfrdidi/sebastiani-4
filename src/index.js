a10c368c9afec3b6d6c48861744a0046ab80ae52
Lukas Hahmann <tfr.didi@gmail.com>
2023-01-06 01:15:46 +0100

    animation gefixt

--- src/index.js
+++ src/index.js
@@ -9,25 +9,27 @@ console.log("🦊 Hello! Edit me in src/index.js");
 // timeline
 
 // import Swiper JS
+// https://github.com/nolimits4web/Swiper/blob/Swiper3/API.md
 import Swiper from './js/swiper';
 
 var swiper = new Swiper(".swiper-container", {
     direction: 'vertical',
-  loop: false,
-  speed: 1600,
-  pagination: '.swiper-pagination',
-  paginationBulletRender: function (swiper, index, className) {
-    var year = document.querySelectorAll('.swiper-slide')[index].getAttribute('data-year');
-    return '<span class="' + className + '">' + year + '</span>';
-  },
-  paginationClickable: true,
-  nextButton: '.swiper-button-next',
-  prevButton: '.swiper-button-prev',
-  breakpoints: {
-    768: {
-      direction: 'horizontal',
+    loop: false,
+    speed: 600,
+    pagination: '.swiper-pagination',
+    effect: 'fade',
+    paginationBulletRender: function (swiper, index, className) {
+        var year = document.querySelectorAll('.swiper-slide')[index].getAttribute('data-year');
+        return '<span class="' + className + '">' + year + '</span>';
+    },
+    paginationClickable: true,
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    breakpoints: {
+        768: {
+            direction: 'horizontal',
+        }
     }
-  }
 });
   
   console.log("🦊 Hello from didi");
